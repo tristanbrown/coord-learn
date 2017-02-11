6843a9fabaa5a28c6ea2ba24183f32e2137d1d02
tristanbrown <brown.tristan.r@gmail.com>
2017-02-10 18:41:47 -0600

    Molecule xyz extraction

--- moldata.py
+++ moldata.py
@@ -1,43 +1,39 @@
+from ccdc import io
 import pandas as pd
 
-print(pd.__file__)
-
-from ccdc import io
-
 csd_reader = io.EntryReader('CSD')
-entry_abebuf = csd_reader.entry('ABEBUF')
-cryst_abebuf = csd_reader.crystal('ABEBUF')
-mol_abebuf = csd_reader.molecule('ABEBUF')
 
-print(round(mol_abebuf.molecular_weight, 3))
+class Molecule():
+    """
+    """
+    def __init__(self, entry):
+        self.entry = entry
 
-reader_formats = io.MoleculeReader.known_formats.keys()
-reader_formats.sort()
-for format in reader_formats:
-    print format
+    def xyz(self):
+        mol = csd_reader.molecule(self.entry)
+        size = len(mol.atoms)
+        coords = []
+        for i in range(size):
+            atom = mol.atoms[i]
+            coords.append([atom.atomic_symbol] + 
+                [round(a, 4) for a in atom.coordinates])
+            #use atom.label to get the symbol and index number together
+        return pd.DataFrame(coords, columns=['Element', 'x', 'y', 'z'])
 
-first_molecule = csd_reader[0]
-print first_molecule.identifier
-ababub = csd_reader.entry('ABABUB')
-mol = ababub.molecule
-print len(mol.atoms)
-print mol.formula
+abebuf = Molecule('ABEBUF')
+print(abebuf.xyz())
+print(len(csd_reader))
 
-for i in range(11):
-    mol = csd_reader[i]
-    print mol.identifier
+class Molset():
+    """
+    """
+    def __init__(self):
+        self.xyzset = {}
+        
+    def populatexyz(self, count):
+        self.xyzset = {csd_reader[m].identifier: 
+                        Molecule(csd_reader[m]).xyz() for m in range(count)}
 
-mol = csd_reader.molecule('ABEBUF')
-size = len(mol.atoms)
-
-def listcoords(entry):
-    mol = csd_reader.molecule(entry)
-    size = len(mol.atoms)
-    structure = []
-    for i in range(size):
-        atom = mol.atoms[i]
-        structure.append([atom.atomic_symbol] + [round(a, 4) for a in atom.coordinates])
-        #use atom.label to get the symbol and index number together
-    return pd.DataFrame(structure, columns=['Element', 'X', 'Y', 'Z'])
-
-print(listcoords('ABEBUF'))
+trainset = Molset()
+trainset.populatexyz(5)
+print(trainset.xyzset)
