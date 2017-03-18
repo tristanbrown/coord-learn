4c893f5d93401d4d11abee68d3b28bee53e02c98
tristanbrown <brown.tristan.r@gmail.com>
2017-03-18 16:26:05 -0500

    Organized and labeled Perceptron training functions in _train02.py.

--- _train02.py
+++ _train02.py
@@ -4,75 +4,36 @@
 from moldata import *
     
 ## Data prep. 
-trainset1 = Molset(1000, 'C', 20)
+trainset1 = Molset(200, 'C', 20)
 print(trainset1.X)
 print(trainset1.y)
 print([(len(trainset1.X), len(trainset1.X[0])), len(trainset1.y)])
 
 ## Training.
+# Split the data.
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
         trainset1.X, trainset1.y, test_size=0.3, random_state=0)
 
+# Standardize the data to a normal distribution.
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
 
+# Train on the standardized data. 
 from sklearn.linear_model import Perceptron
 ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
 ppn.fit(X_train_std, y_train)
 
+# Apply the trained network to the standardized test data and print metrics.
 y_pred = ppn.predict(X_test_std)
 print('Misclassified samples: %d' % (y_test != y_pred).sum())
 
 from sklearn.metrics import accuracy_score
-print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
 
 ## Plotting
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 
-def plot_decision_regions(X, y, classifier, 
-                                test_idx=None, resolution=0.02):
-                                
-    # setup marker generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-    
-    # plot the decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                                np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-    
-    # plot all samples
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
-                    alpha=0.8, c=cmap(idx),
-                    marker=markers[idx], label=cl)
-    
-    # highlight test samples
-    if test_idx:
-        X_test, y_test = X[test_idx, :], y[test_idx]
-        plt.scatter(X_test[:, 0], X_test[:, 1], c='',
-                alpha=1.0, linewidths=1, marker='o',
-                s=55, label='test set')
-        
-X_combined_std = np.vstack((X_train_std, X_test_std))
-y_combined = np.hstack((y_train, y_test))
-plot_decision_regions(X=X_combined_std,
-                        y=y_combined,
-                        classifier=ppn,
-                        test_idx=range(105,150))
-plt.xlabel('x')
-plt.ylabel('y')
-plt.legend(loc='upper left')
-plt.show()
