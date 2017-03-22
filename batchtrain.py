543e8a52733b73e75b46c85e3dc9562184a71b46
tristanbrown <brown.tristan.r@gmail.com>
2017-03-22 18:13:47 -0500

    Wrote most of train() function in BatchTrainer.

--- batchtrain.py
+++ batchtrain.py
@@ -3,6 +3,8 @@
 
 from moldata import *
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 class BatchTrainer():
     """
@@ -14,15 +16,30 @@ class BatchTrainer():
     The accuracy values are stored in a [ordered dict or dataframe?], so they
     can be saved to a .csv file and plotted (accuracy vs element). 
     """
-    def __init__(self):
+    def __init__(self, sample_size=100, closest_atoms=20, test_split=0.3):
         self.Periodic_Table = pd.read_csv('element_data.csv',
                                         delimiter=',', header=0, index_col=0)
         self.NN = Perceptron(n_iter=40, eta0=0.1, random_state=0)
         self.accuracy = [] # Ordered dictionary vs dataframe?
+        self.samples = sample_size
+        self.range = closest_atoms
+        self.split = test_split
     
     def train(self, element):
         """"""
+        trainset = Molset(self.samples, element, self.range)
+        X_train, X_test, y_train, y_test = train_test_split(
+            trainset.X, trainset.y, test_size=self.split, random_state=0)
+        sc = StandardScaler()
+        sc.fit(X_train)
+        X_train_std = sc.transform(X_train)
+        X_test_std = sc.transform(X_test)
         
+        self.NN.fit(X_train_std, y_train)
+        y_pred = ppn.predict(X_test_std)
+        
+        # Create NN Accuracy column in self.Periodic_Table.
+        # Insert accuracy_score(y_test, y_pred) into NN Accuracy column.
 
 trainset1 = Molset(200, 'C', 20)
 print(trainset1.X)
